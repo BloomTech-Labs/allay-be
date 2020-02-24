6bd9be8250feffcd05cc7d627f6ae509cf30c1e6
taterntots <matthew.a.weidner@gmail.com>
2020-02-24 12:48:33 -0500

    got my tests running on the test database

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const knex = require('knex');
 const config = require('../knexfile');
 
-const dbEnv = process.env.DB_ENV || 'development';
+const dbEnv = process.env.DB_ENV || 'testing';
 
 module.exports = knex(config[dbEnv]);
