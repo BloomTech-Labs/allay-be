73ab6a9cd28f32909f6ffc84887b9091825fcb16
Shawn DelPercio <sdelperc@gmail.com>
2020-04-21 13:34:46 -0400

    added endpoint to update a users blocked status; updated findUserById model function to include blocked status

--- helpers/users-model.js
+++ helpers/users-model.js
@@ -1,159 +1,157 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-  findUsers,
-  findUsersBy,
-  findUserById,
-  findUserReviews,
-  findUserReviewsById,
-  addUser,
-  updateUser,
-  deleteUser
+	findUsers,
+	findUsersBy,
+	findUserById,
+	findUserReviews,
+	findUserReviewsById,
+	addUser,
+	updateUser,
+	deleteUser,
 };
 
 // FIND ALL USERS
 function findUsers() {
-  return db('users');
+	return db('users');
 }
 
 // FIND USERS BY A SPECIFIC FILTER (MUST BE A COLUMN IN THE USERS TABLE AND USE {<ARGUMENT>})
 function findUsersBy(filter) {
-  return db('users').where(filter);
+	return db('users').where(filter);
 }
 
 // FIND USER BY ID, WILL CONTAIN ANY REVIEWS ASSOCIATED WITH THE USER OR AN EMPTY ARRAY
 function findUserById(userId) {
-  return db('users as u')
-    .where('id', userId)
-    .select('u.id', 'u.username', 'u.email', 'u.track_id')
-    .first()
-    .then(user => {
-      return findUserReviews(user.id).then(userReviews => {
-        return {
-          ...user,
-          reviews: userReviews
-        };
-      });
-    });
+	return db('users as u')
+		.where('id', userId)
+		.select('u.id', 'u.username', 'u.email', 'u.track_id', 'u.blocked')
+		.first()
+		.then((user) => {
+			return findUserReviews(user.id).then((userReviews) => {
+				return {
+					...user,
+					reviews: userReviews,
+				};
+			});
+		});
 }
 
 // FIND ONLY THE COMPANY REVIEWS ASSOCIATED WITH A USER
 
 function findUserReviews(userId) {
-  return db('reviews as r')
-    .select(
-      'r.id as review_id',
-      'u.id as user_id',
-      'u.username',
-      't.track_name',
-      'rt.review_type',
-      'c.company_name',
-      'c.domain as logo',
-      'ws.work_status ',
-      'r.job_title',
-      'r.city',
-      's.state_name',
-      'r.start_date',
-      'r.end_date',
-      'r.interview_rounds',
-      'r.phone_interview',
-      'r.resume_review',
-      'r.take_home_assignments',
-      'r.online_coding_assignments',
-      'r.portfolio_review',
-      'r.screen_share',
-      'r.open_source_contribution',
-      'r.side_projects',
-      'r.online_coding_assignments',
-      'r.comment',
-      'r.typical_hours',
-      'r.salary',
-      'r.difficulty_rating',
-      'os.offer_status',
-      'r.overall_rating',
-      'r.created_at',
-      'r.updated_at'
-    )
-    .where('r.user_id', userId)
-    .join('users as u', 'r.user_id', 'u.id')
-    .join('tracks as t', 'u.track_id', 't.id')
-    .join('companies as c', 'r.company_name', 'c.company_name')
-    .join('work_status as ws', 'r.work_status_id', 'ws.id')
-    .join('offer_status as os', 'r.offer_status_id', 'os.id')
-    .join('states as s', 'r.state_id', 's.id')
-    .join('review_types as rt', 'r.review_type_id', 'rt.id');
+	return db('reviews as r')
+		.select(
+			'r.id as review_id',
+			'u.id as user_id',
+			'u.username',
+			't.track_name',
+			'rt.review_type',
+			'c.company_name',
+			'c.domain as logo',
+			'ws.work_status ',
+			'r.job_title',
+			'r.city',
+			's.state_name',
+			'r.start_date',
+			'r.end_date',
+			'r.interview_rounds',
+			'r.phone_interview',
+			'r.resume_review',
+			'r.take_home_assignments',
+			'r.online_coding_assignments',
+			'r.portfolio_review',
+			'r.screen_share',
+			'r.open_source_contribution',
+			'r.side_projects',
+			'r.online_coding_assignments',
+			'r.comment',
+			'r.typical_hours',
+			'r.salary',
+			'r.difficulty_rating',
+			'os.offer_status',
+			'r.overall_rating',
+			'r.created_at',
+			'r.updated_at'
+		)
+		.where('r.user_id', userId)
+		.join('users as u', 'r.user_id', 'u.id')
+		.join('tracks as t', 'u.track_id', 't.id')
+		.join('companies as c', 'r.company_name', 'c.company_name')
+		.join('work_status as ws', 'r.work_status_id', 'ws.id')
+		.join('offer_status as os', 'r.offer_status_id', 'os.id')
+		.join('states as s', 'r.state_id', 's.id')
+		.join('review_types as rt', 'r.review_type_id', 'rt.id');
 }
 
 // FIND A SINGLE COMPANY REVIEW ASSOCIATED WITH A USER
 
 function findUserReviewsById(revId) {
-  return db('reviews as r')
-    .select(
-      'r.id as review_id',
-      'u.id as user_id',
-      'u.username',
-      't.track_name',
-      'rt.review_type',
-      'c.company_name',
-      'c.domain as logo',
-      'ws.work_status ',
-      'r.job_title',
-      'r.city',
-      's.state_name',
-      'r.start_date',
-      'r.end_date',
-      'r.interview_rounds',
-      'r.phone_interview',
-      'r.resume_review',
-      'r.take_home_assignments',
-      'r.online_coding_assignments',
-      'r.portfolio_review',
-      'r.screen_share',
-      'r.open_source_contribution',
-      'r.side_projects',
-      'r.online_coding_assignments',
-      'r.comment',
-      'r.typical_hours',
-      'r.salary',
-      'r.difficulty_rating',
-      'os.offer_status',
-      'r.overall_rating',
-      'r.created_at',
-      'r.updated_at'
-    )
-    .where('r.id', revId)
-    .join('users as u', 'r.user_id', 'u.id')
-    .join('tracks as t', 'u.track_id', 't.id')
-    .join('companies as c', 'r.company_name', 'c.company_name')
-    .join('work_status as ws', 'r.work_status_id', 'ws.id')
-    .join('offer_status as os', 'r.offer_status_id', 'os.id')
-    .join('states as s', 'r.state_id', 's.id')
-    .join('review_types as rt', 'r.review_type_id', 'rt.id');
+	return db('reviews as r')
+		.select(
+			'r.id as review_id',
+			'u.id as user_id',
+			'u.username',
+			't.track_name',
+			'rt.review_type',
+			'c.company_name',
+			'c.domain as logo',
+			'ws.work_status ',
+			'r.job_title',
+			'r.city',
+			's.state_name',
+			'r.start_date',
+			'r.end_date',
+			'r.interview_rounds',
+			'r.phone_interview',
+			'r.resume_review',
+			'r.take_home_assignments',
+			'r.online_coding_assignments',
+			'r.portfolio_review',
+			'r.screen_share',
+			'r.open_source_contribution',
+			'r.side_projects',
+			'r.online_coding_assignments',
+			'r.comment',
+			'r.typical_hours',
+			'r.salary',
+			'r.difficulty_rating',
+			'os.offer_status',
+			'r.overall_rating',
+			'r.created_at',
+			'r.updated_at'
+		)
+		.where('r.id', revId)
+		.join('users as u', 'r.user_id', 'u.id')
+		.join('tracks as t', 'u.track_id', 't.id')
+		.join('companies as c', 'r.company_name', 'c.company_name')
+		.join('work_status as ws', 'r.work_status_id', 'ws.id')
+		.join('offer_status as os', 'r.offer_status_id', 'os.id')
+		.join('states as s', 'r.state_id', 's.id')
+		.join('review_types as rt', 'r.review_type_id', 'rt.id');
 }
 
 // ADD A USER TO THE DATABASE
 
 function addUser(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(([id]) => {
-      return findUserById(id);
-    });
+	return db('users')
+		.insert(user, 'id')
+		.then(([id]) => {
+			return findUserById(id);
+		});
 }
 
 // UPDATE AN EXISTING USER
 
 function updateUser(id, changes) {
-  return db('users')
-    .where({ id })
-    .update(changes)
-    .then(count => (count > 0 ? findUserById(id) : null));
+	return db('users')
+		.where({ id })
+		.update(changes)
+		.then((count) => (count > 0 ? findUserById(id) : null));
 }
 
 // DELETE AN EXISTING USER
 
 function deleteUser(id) {
-  return db('users')
-    .where({ id })
-    .del();
+	return db('users').where({ id }).del();
 }
--- routers/users-router.js
+++ routers/users-router.js
@@ -4,9 +4,9 @@ const User = require('../helpers/users-model.js');
 const Revs = require('../helpers/reviews-model.js');
 
 const {
-  validateUserId,
-  checkForReviewData,
-  validateReviewId
+	validateUserId,
+	checkForReviewData,
+	validateReviewId,
 } = require('../middleware/index.js');
 
 /**************************************************************************/
@@ -17,79 +17,102 @@ const {
 
 //*************** GET ALL USERS *****************// - Remove for production or create new auth for admin only access to this endpoint
 router.get('/all', (req, res) => {
-  User.findUsers()
-    .then(user => {
-      res.json(user);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: 'There was an error getting all users to dsiplay'
-      });
-    });
+	User.findUsers()
+		.then((user) => {
+			res.json(user);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: 'There was an error getting all users to dsiplay',
+			});
+		});
 });
 
 //*************** GET USER BY ID *****************//
 router.get('/:userId', validateUserId, (req, res) => {
-  const { userId } = req.params;
-  User.findUserById(userId)
-    .then(user => {
-      res.json(user);
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'There was an error getting user' });
-    });
+	const { userId } = req.params;
+	User.findUserById(userId)
+		.then((user) => {
+			res.json(user);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'There was an error getting user' });
+		});
 });
 
 //*************** UPDATE USER INFO ******************//
 router.put('/:userId', validateUserId, (req, res) => {
-  const { email, password, username, track_id } = req.body;
-  const { userId } = req.params;
-  id = userId;
-  User.findUsersBy({ id }).then(userInfo => {
-    const user = userInfo;
-    if (
-      email === user[0].email &&
-      username === user[0].username &&
-      password === user[0].password &&
-      track_id === user[0].track_id
-    ) {
-      return res.status(200).json({ message: 'No changes to update' });
-    } else {
-      User.updateUser(id, { email, password, username, track_id })
-        .then(updatedInfo => {
-          if (updatedInfo) {
-            res
-              .status(202)
-              .json({ updatedInfo: { email, password, username, track_id } });
-          } else {
-            res.status(404).json({ message: 'Error locating user info' });
-          }
-        })
-        .catch(err => {
-          res.status(500).json({ message: 'Error updating user info' });
-        });
-    }
-  });
+	const { email, password, username, track_id } = req.body;
+	const { userId } = req.params;
+	id = userId;
+	User.findUsersBy({ id }).then((userInfo) => {
+		const user = userInfo;
+		if (
+			email === user[0].email &&
+			username === user[0].username &&
+			password === user[0].password &&
+			track_id === user[0].track_id
+		) {
+			return res.status(200).json({ message: 'No changes to update' });
+		} else {
+			User.updateUser(id, { email, password, username, track_id })
+				.then((updatedInfo) => {
+					if (updatedInfo) {
+						res
+							.status(202)
+							.json({ updatedInfo: { email, password, username, track_id } });
+					} else {
+						res.status(404).json({ message: 'Error locating user info' });
+					}
+				})
+				.catch((err) => {
+					res.status(500).json({ message: 'Error updating user info' });
+				});
+		}
+	});
+});
+
+//*************** UPDATE USER BIND STATUS ******************//
+router.put('/:userId/bind', validateUserId, (req, res) => {
+	const { userId } = req.params;
+
+	User.findUserById(userId).then((userInfo) => {
+		if (userInfo) {
+			User.updateUser(userId, { blocked: !userInfo.blocked })
+				.then((updatedInfo) => {
+					if (updatedInfo) {
+						res.status(202).json({ updatedInfo: { updatedInfo } });
+					} else {
+						res.status(404).json({ message: 'Error locating user info' });
+					}
+				})
+				.catch((err) => {
+					res.status(500).json({ message: 'Error updating user info' });
+				});
+		} else {
+			res.status(404).json({ message: 'Error locating user info' });
+		}
+	});
 });
 
 //****************** DELETE ACCOUNT ********************//
 router.delete('/:userId', async (req, res) => {
-  const { userId } = req.params;
+	const { userId } = req.params;
 
-  try {
-    const user = await User.findUserById(userId);
-    if (user) {
-      const deleted = await User.deleteUser(userId);
-      res.status(200).json({ message: 'User account deleted' });
-    } else {
-      res.status(404).json({ message: 'Error locating user.' });
-    }
-  } catch {
-    res.status(500).json({
-      message: 'There was an error deleting this account.'
-    });
-  }
+	try {
+		const user = await User.findUserById(userId);
+		if (user) {
+			const deleted = await User.deleteUser(userId);
+			res.status(200).json({ message: 'User account deleted' });
+		} else {
+			res.status(404).json({ message: 'Error locating user.' });
+		}
+	} catch {
+		res.status(500).json({
+			message: 'There was an error deleting this account.',
+		});
+	}
 });
 
 /**************************************************************************/
@@ -101,110 +124,110 @@ router.delete('/:userId', async (req, res) => {
 //***************** GET USERS REVIEWS *******************//
 
 router.get('/:userId/reviews', validateUserId, (req, res) => {
-  const { userId } = req.params;
-  User.findUserReviews(userId)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: 'Server was not able to retrieve user reviews'
-      });
-    });
+	const { userId } = req.params;
+	User.findUserReviews(userId)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: 'Server was not able to retrieve user reviews',
+			});
+		});
 });
 
 //************* GET A SINGLE REVIEW BY USER ID ***************//
 
 router.get(
-  '/:userId/reviews/:revId',
-  validateUserId,
-  validateReviewId,
-  (req, res) => {
-    const { revId } = req.params;
-    User.findUserReviewsById(revId)
-      .then(review => {
-        res.status(200).json(review);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: 'Could not retrieve single user review.' });
-      });
-  }
+	'/:userId/reviews/:revId',
+	validateUserId,
+	validateReviewId,
+	(req, res) => {
+		const { revId } = req.params;
+		User.findUserReviewsById(revId)
+			.then((review) => {
+				res.status(200).json(review);
+			})
+			.catch((err) => {
+				res
+					.status(500)
+					.json({ error: 'Could not retrieve single user review.' });
+			});
+	}
 );
 
 //***************** ADD NEW REVIEW *******************// ===== make sure to update the if else statement====
 router.post(
-  '/:userId/add-review',
-  checkForReviewData,
-  validateUserId,
-  (req, res) => {
-    const { userId } = req.params;
+	'/:userId/add-review',
+	checkForReviewData,
+	validateUserId,
+	(req, res) => {
+		const { userId } = req.params;
 
-    let review = req.body;
-    review = { ...review, user_id: userId };
+		let review = req.body;
+		review = { ...review, user_id: userId };
 
-    if (Number(req.user.id) === Number(userId)) {
-      Revs.addReview(review)
-        .then(newReview => {
-          res.status(201).json(newReview);
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: 'There was an error check id or review fields'
-          });
-        });
-    } else {
-      return res.status(404).json({ error: 'Wrong user' });
-    }
-  }
+		if (Number(req.user.id) === Number(userId)) {
+			Revs.addReview(review)
+				.then((newReview) => {
+					res.status(201).json(newReview);
+				})
+				.catch((err) => {
+					res.status(500).json({
+						error: 'There was an error check id or review fields',
+					});
+				});
+		} else {
+			return res.status(404).json({ error: 'Wrong user' });
+		}
+	}
 );
 
 //************* EDIT A REVIEW WITH USER ID ***************//
 
 router.put(
-  '/:userId/reviews/:revId',
-  validateUserId,
-  validateReviewId,
-  (req, res) => {
-    const { revId } = req.params;
+	'/:userId/reviews/:revId',
+	validateUserId,
+	validateReviewId,
+	(req, res) => {
+		const { revId } = req.params;
 
-    const changes = req.body;
+		const changes = req.body;
 
-    Revs.updateReview(revId, changes)
-      .then(updatedReview => {
-        if (updatedReview) {
-          res.status(200).json({ updatedReview: changes });
-        } else {
-          res.status(404).json({
-            error: 'could not find a valid review'
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'can not edit review' });
-      });
-  }
+		Revs.updateReview(revId, changes)
+			.then((updatedReview) => {
+				if (updatedReview) {
+					res.status(200).json({ updatedReview: changes });
+				} else {
+					res.status(404).json({
+						error: 'could not find a valid review',
+					});
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({ error: 'can not edit review' });
+			});
+	}
 );
 
 //************* DELETE A REVIEW BY USER ID ***************//
 
 router.delete(
-  '/:userId/reviews/:revId',
-  validateUserId,
-  validateReviewId,
-  (req, res) => {
-    const { revId } = req.params;
-    Revs.deleteReview(revId)
-      .then(deleted => {
-        res.status(200).json(deleted);
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: ' was not able to delete interview review'
-        });
-      });
-  }
+	'/:userId/reviews/:revId',
+	validateUserId,
+	validateReviewId,
+	(req, res) => {
+		const { revId } = req.params;
+		Revs.deleteReview(revId)
+			.then((deleted) => {
+				res.status(200).json(deleted);
+			})
+			.catch((err) => {
+				res.status(500).json({
+					error: ' was not able to delete interview review',
+				});
+			});
+	}
 );
 
 /**************************************************************************/
